6050bd6f325ece9387dc77ff16f63918133ab32d
patelkd <karishma1126@yahoo.com>
2013-12-09 17:43:42 -0700

    f

--- Students.java
+++ Students.java
@@ -4,31 +4,69 @@ import java.io.*;
 import java.awt.*;
 
 public class Students {
-	public static void main(String[] args) throws IOException {
-		Random random = new Random();
+        public static void main(String[] args) throws IOException {
+                Random random = new Random();
 
-		File file = new File("class.csv");
-		Scanner scan = new Scanner (file);
-		String[] names = scan.nextLine().split(",");
+                File file = new File("class.csv");
+                Scanner scan = new Scanner(System.in);
+                Scanner reader = new Scanner (file);
+                String[] names = reader.nextLine().split(",");
+                //int[] used = new int[30];
 
-		for (int i=0; i<names.length; i++)  {
-			Random rand = new Random();
-			int ran = random.nextInt(names.length);
-			System.out.println(names[ran]);
-		}
+        for (int i=names.length-1; i>0; i--) {
+               // used[i] = i;
+                String a = names[i];
+                int b = random.nextInt(i);
+                names[i] = names[b];
+                names[b] = a;
 
-		// System.out.println("How many groups do you want?");
-		// int groups = reader.nextInt();
 
-		// if (number.length % group == 0) {
-		// 	int people = number.length / group;
+        }
+        
+        int randomStudent = random.nextInt(names.length);
+        System.out.println("The random student is " + names[randomStudent]);
+        System.out.println();
 
-		// 	for (j=0; j<groups; j++) {
-		// 		for (int i=0; i<people; i++) {
-					
-		// 		}
-		// 	}
-			
-		// }
-	}
+                System.out.println("How many groups do you want?");
+                int groups = scan.nextInt();
+            
+                //int groups = 5;
+                if (names.length % groups == 0) {
+               
+                        int people = names.length / groups;
+                        String[][] groupedNames = new String[groups][people];
+                        int counter = 0;
+                        for (int j=0; j<groups; j++) {
+                                for (int i=0; i<people; i++) {
+                                     groupedNames[j][i] = names[counter];
+                                     counter = counter + 1;
+                                }
+                        }
+                        for (int i=0; i<people; i++) {
+                           System.out.println(groupedNames[3][i]);     
+                        }
+                        
+                } else if (names.length % groups != 0) {
+                   int people = names.length / groups;
+                   int remainder = names.length % groups;   
+                   int count = 0;
+                   String[] evenNames = new String[groups];
+
+
+                    for (int i=0; i<groups; i++) {
+                        for (int j=0; j<people; j++) {
+                            evenNames[i] = names[count];
+                            
+                            System.out.println(evenNames[i] + " ");
+                            count++;
+                            
+                        }
+                    System.out.println();
+                    
+                            }
+                        if (remainder > 0) {
+                        System.out.println("Amount of people who are not in a group: " + remainder);
+                    }
+                }
+            }
 }
